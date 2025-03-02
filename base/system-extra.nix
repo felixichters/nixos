b3899778dcacfea7b65b424487d187dae68b9890
felix <>
2025-03-02 21:01:04 +0100

    more cleanup

--- base/system-extra.nix
+++ base/system-extra.nix
@@ -8,14 +8,10 @@
 	
 	programs.appimage.enable = true;	
 	
-	#services.udisks2.enable = true;
-	
 	programs.wireshark.enable = true;
 	
 	virtualisation.docker.enable = true;
 
-	programs.gnome-disks.enable = true;
-	
 	programs.virt-manager.enable = true;
 	virtualisation.libvirtd.enable = true;
 	virtualisation.spiceUSBRedirection.enable = true;
