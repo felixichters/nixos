e2fe046d57b43359c372b7ddc2d40a27fac6aa5c
felix <ichters.fe@gmail.com>
2025-03-20 12:29:42 +0100

    l13y reinstall

--- hosts/l13y/configuration.nix
+++ hosts/l13y/configuration.nix
@@ -6,8 +6,8 @@
 		./boot.nix
 		../../modules/system/system.nix
 	];
-	options.gaming.enable = true;
+	#options.gaming.enable = false;
 	nixpkgs.config.allowUnfree = true;
-	system.stateVersion = "23.11";
+	system.stateVersion = "24.11";
 	nix.settings.experimental-features = ["nix-command" "flakes"];
 }
--- hosts/l13y/hardware-configuration.nix
+++ hosts/l13y/hardware-configuration.nix
@@ -4,24 +4,24 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-	imports =
+  imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e323da23-ce3f-46b7-b4cd-813a52a0bf13";
+    { device = "/dev/disk/by-uuid/0de3c5ed-9e90-48c8-b95e-ede9868db219";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/941E-DD2E";
+    { device = "/dev/disk/by-uuid/9AF3-5348";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices = [ ];
