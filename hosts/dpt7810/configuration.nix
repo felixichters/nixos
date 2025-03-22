73dd5483f2ffe8cdbc95101e04a7b002f7d0b071
felix <ichters.fe@gmail.com>
2025-03-22 12:59:50 +0100

    dpt7810 nvidia drivers

--- hosts/dpt7810/configuration.nix
+++ hosts/dpt7810/configuration.nix
@@ -1,12 +1,13 @@
-{ config, pkgs, ... }:
+{... }:
 
 {
 	imports = [
 		./boot.nix
+		./nvidia.nix
 		./hardware-configuration.nix
 		../../modules/system/system.nix
 	];
-	
+	gaming.enable = true;	
 	nixpkgs.config.allowUnfree = true;
 	system.stateVersion = "24.11";
 	nix.settings.experimental-features = ["nix-command" "flakes"];
