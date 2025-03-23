1209384fbbc3fec1b8e732fd49d324e8383c8e99
felix <ichters.fe@gmail.com>
2025-03-23 18:41:57 +0100

    added multi theme feature

--- hosts/l13y/flake.nix
+++ hosts/l13y/flake.nix
@@ -17,14 +17,69 @@
 			lib = nixpkgs.lib;
 			system = "x86_64-linux";
 			pkgs = nixpkgs.legacyPackages.${system};
+			themes = {
+				dark = {
+					background = "#101010";
+					background_alt = "#191919";
+					foreground = "#cccccc";
+					selection_bg = "#cccccc";
+					selection_fg = "#101010";
+					border_focused = "#708090";
+					border_unfocused = "#2a2a2a";
+					wallpaper = "#191919";
+					
+					color00 = "#080808";
+					color01 = "#d70000";
+					color02 = "#789978";
+					color03 = "#ffaa88";
+					color04 = "#708090";
+					color05 = "#789978";
+					color06 = "#7788aa";
+					color07 = "#dddddd";
+					color08 = "#555555";
+					color09 = "#ffaa88";
+					color10 = "#2a2a2a";
+					color11 = "#444444";
+					color12 = "#aaaaaa";
+					color13 = "#dddddd";
+					color14 = "#7788aa";
+					color15 = "#deeeed";
+				};
+				light = {
+					background = "#eff1f5";
+					background_alt = "#e6e9ef";
+					foreground = "#4c4f69";
+					selection_bg = "#dc8a78";
+					selection_fg = "#eff1f5";
+					border_unfocused = "#dce0e8";
+					border_focused = "#bcc0cc";
+					wallpaper = "#e6e9ef";
+
+					color00 = "#5c5f77";
+					color01 = "#d20f39";
+					color02 = "#40a02b";
+					color03 = "#df8e1d";
+					color04 = "#1e66f5";
+					color05 = "#ea76cb";
+					color06 = "#179299";
+					color07 = "#acb0be";
+					color08 = "#6c6f85";
+					color09 = "#d20f39";
+					color10 = "#40a02b";
+					color11 = "#df8e1d";
+					color12 = "#1e66f5";
+					color13 = "#ea76cb";
+					color14 = "#179299";
+					color15 = "#bcc0cc";
+				};
+			};
+		theme = themes.light;
 		in {
 		nixosConfigurations = {
 			${host} = lib.nixosSystem {
 				inherit system;
 				specialArgs = { 
-					inherit inputs;
-					inherit host;
-					inherit user;
+					inherit inputs host user theme;
 				};
 				modules = [
 					./configuration.nix
@@ -35,9 +90,7 @@
 			${user} = home-manager.lib.homeManagerConfiguration {
 				inherit pkgs;
 				extraSpecialArgs = { 
-					inherit inputs;
-					inherit host;
-					inherit user;
+					inherit inputs host user theme;
 				};
 				modules = [
 					./home.nix
--- modules/home/core/session/sway/sway.nix
+++ modules/home/core/session/sway/sway.nix
@@ -1,4 +1,4 @@
-{config, lib, pkgs, ...}:
+{config, lib, pkgs, theme, ...}:
 {
 	options.sway.enable = lib.mkOption {
 		type = lib.types.bool;
@@ -21,7 +21,9 @@
 				window.titlebar = false;
 				window.border = 2;
 				gaps = {
+					inner = 10;
 					smartBorders = "on";
+					smartGaps = true;
 				};
 				input = {
 					"*" = {
@@ -35,125 +37,42 @@
 					names = ["IBM Plex Mono"];
 					size = 10.0;
 				};
-				output."*".bg = "#708090 solid_color";
+				output."*".bg = "${theme.wallpaper} solid_color";
 				bars = [{
-					#position = "top";
 					command = "${pkgs.waybar}/bin/waybar";
-					#statusCommand = "while ~/.dotfiles/home/core/session/sway/status.sh; do sleep 1; done";
-					#trayOutput="none";
-					#fonts = {
-					#	names = ["Iosevka"];
-					#	size = 11.0;
-					#};
-					#colors = {
-					#	background = "#191919";
-					#	statusline = "#aaaaaa";
-					#	separator = "#7a7a7a";
-					#	focusedWorkspace = {
-					#		background = "#708090";
-					#		border = "708090";
-					#		text = "cccccc";
-					#	};
-					#	activeWorkspace = {
-					#		background = "#444444";
-					#		border = "#789978";
-					#		text = "aaaaaa";
-					#	};
-					#	inactiveWorkspace = {
-					#		background = "#2a2a2a";
-					#		border = "#191919";
-					#		text = "#7a7a7a";
-					#	};
-					#};
 				}];
 				colors = {
-					background = "#101010";
+					background = theme.background;
 					focused = {
-						border = "#708090";
-						background = "#708090";
-						text = "#cccccc";
-						indicator = "#708090";
-						childBorder = "#708090";
+						border = theme.border_focused;
+						background = theme.border_focused;
+						text = theme.foreground;
+						indicator = theme.border_focused;
+						childBorder = theme.border_focused;
 					};
 					focusedInactive = {
-						border = "#708090";
-						background = "#7a7a7a";
-						text = "#cccccc";
-						indicator = "#708090";
-						childBorder = "#708090";
+						border = theme.border_unfocused;
+						background = theme.border_unfocused;
+						text = theme.foreground;
+						indicator = theme.border_unfocused;
+						childBorder = theme.border_unfocused;
 					};
 					unfocused = {
-						border = "#2a2a2a";
-						background = "#191919";
-						text = "#aaaaaa";
-						indicator = "#2a2a2a";
-						childBorder = "#2a2a2a";
+						border = theme.border_unfocused;
+						background = theme.border_unfocused;
+						text = theme.foreground;
+						indicator = theme.border_unfocused;
+						childBorder = theme.border_unfocused;
 					};
 					urgent = {
-						border = "d70000";
-						background = "ffaa88";
-						text = "deeeed";
-						indicator = "d70000";
-						childBorder = "d70000";
+						border = theme.color01;
+						background = theme.color01;
+						text = theme.foreground;
+						indicator = theme.color01;
+						childBorder = theme.color01;
 					};
 				};
 			};
 		};
-		#programs.i3status = {
-		#	enable = true;
-		#	enableDefault = false;
-		#	general = {
-		#		colors = true;
-		#		color_good = "#789978";
-		#		color_degraded = "#ff88aa";
-		#		color_bad = "#d70000";
-		#		interval = 5;
-		#	};
-		#	modules = {
-		#		"time" = {
-		#			position = 6;
-		#			settings = {
-		#				format = "%Y-%m-%d %H:%M";
-		#			};
-		#		};
-		#		"battery 0" = {
-		#			position = 5;
-		#			settings = {
-		#				format = "%status %percentage";
-		#				low_threshold = "30";
-		#			};
-		#		};
-		#		"wireless _first_" = {
-		#			position = 3;
-		#			settings = {
-		#				format_up = "(%bitrate %essid) %ip";
-		#				format_down = "down";
-		#			};
-		#		};
-		#		"volume master" = {
-    #			position = 4;
-    #			settings = {
-    #  			format = "VOL %volume";
-    #  			format_muted = "MUTE (%volume)";
-    #  			device = "default";
-    #			};
-		#		};
-		#		"memory" = {
-		#			position = 1;
-		#			settings = {
-		#				format = "MEM %used / %total";
-		#				threshold_degraded = "10%";
-		#				threshold_critical = "5%";
-		#			};
-		#		};
-		#		"cpu_usage" = {
-		#			position = 2;
-		#			settings = {
-		#				format = "CPU %usage";
-		#				max_threshold = "75";
-		#			};
-		#		};
-		#	};
-		#};
 	};
 }
