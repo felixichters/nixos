9892c68486006cb293d37bada5d7b84753965395
felix <ichters.fe@gmail.com>
2025-05-25 17:05:31 +0200

    home: update mako config; add extra apps

--- modules/home/apps/mako.nix
+++ modules/home/apps/mako.nix
@@ -9,14 +9,16 @@
 	config = lib.mkIf config.mako.enable {
 		services.mako = { 
 			enable = true;
-			font = "IBM Plex Mono 10";
-			borderSize = 2;
-			borderRadius = 0;
-			ignoreTimeout = true;
-			defaultTimeout = 3000;
-			borderColor = "#2a2a2a";
-			textColor = "#cccccc";
-			backgroundColor = "#191919";
+			settings = {
+				font = "IBM Plex Mono 10";
+				borderSize = 2;
+				borderRadius = 0;
+				ignoreTimeout = true;
+				defaultTimeout = 3000;
+				borderColor = "#2a2a2a";
+				textColor = "#cccccc";
+				backgroundColor = "#191919";
+			};
 		};
 	};
 }
