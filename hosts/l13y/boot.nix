caba9d6de34d763332acdf7175b1ca149fd9d317
felix <ichters.fe@gmail.com>
2025-07-17 22:49:58 +0200

    moved boot to hosts

--- hosts/l13y/boot.nix
+++ hosts/l13y/boot.nix
@@ -1,6 +1,19 @@
-{...}:
+{pkgs, ...}:
 
 {
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;	
+	boot.kernelParams = ["quiet"];
+	#boot.kernelParams = [ "console=tty1" ];
+	services.greetd = {
+		enable = true; 
+		#vt = 2;
+		settings = {                                                           
+			default_session = {                                                  
+				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+				user = "greeter";                                                  
+			};                                                                   
+		};                                                                     
+	};
+
 }
