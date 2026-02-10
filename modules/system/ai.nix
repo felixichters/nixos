74645ab9006ec290be723fadbb1f71cdbfa5ffad
felix <ichters.fe@gmail.com>
2026-02-10 11:59:36 +0100

    system: extend ollama and system config

--- modules/system/ai.nix
+++ modules/system/ai.nix
@@ -1,21 +1,36 @@
 {config, pkgs, lib, ...}:
 let
-  #llama-cpp = pkgs.llama-cpp.override { rocmSupport = true; };
+  llama-cpp = pkgs.llama-cpp;
   llama-server = lib.getExe' llama-cpp "llama-server";
 in
 {
-  environment.systemPackages = [llama-cpp]
+  environment.systemPackages = [llama-cpp];
   services.llama-swap = {
     enable = true;
+    port = 8080;
+    openFirewall = false;
     settings = {
       models = {
-        "qwen2.5-coder" = {
-          cmd = "${llama-server} --port ${PORT} -m /mnt/stash/models/ -ngl 0 -c 4096 --no-webui";
+        "qwen2.5-coder-7b-instruct-q4_k_m" = {
+          cmd = "${llama-server} 
+            --port ${PORT} 
+            -m /mnt/stash/models/qwen2.5-coder-7b-instruct-q4_k_m.gguf 
+            -c 4096 
+            --no-webui";
           aliases = [
-            "qwen1"
+            "qwen-coder"
           ];
         };
-
+        "Qwen2.5-Coder-7B-Instruct-abliterated-Q4_K_M" = {
+          cmd = "${llama-server} 
+            --port ${PORT} 
+            -m /mnt/stash/models/Qwen2.5-Coder-7B-Instruct-abliterated-Q4_K_M.gguf 
+            -c 4096 
+            --no-webui";
+          aliases = [
+            "qwen-coder-abliterated"
+          ];
+        };
       };
     };
   };
