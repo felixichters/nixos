96d209fdfacfb150e7d254deb99b92441a655e71
Felix <f.ichters@proton.me>
2026-02-16 20:48:32 +0100

    bug fixes

--- hosts/l13y/boot.nix
+++ hosts/l13y/boot.nix
@@ -10,7 +10,7 @@
 		#vt = 2;
 		settings = {                                                           
 			default_session = {                                                  
-				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+				command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
 				user = "greeter";                                                  
 			};                                                                   
 		};                                                                     
