605ef7a4e398c235d00cd2b81a613c7b9e645ff5
Felix <ichters.fe@gmail.com>
2026-03-02 12:11:53 +0100

    minor bug fixes

--- hosts/l13y/performance.nix
+++ hosts/l13y/performance.nix
@@ -1,14 +1,14 @@
 {...}:
 
 {
-	services.thermald.enable = true;
+	#services.thermald.enable = true;
 	services.tlp = {
 	enable = true;
 		settings = {
 			CPU_SCALING_GOVERNOR_ON_AC = "performance";
 			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-			CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
-	 		CPU_ENERGY_PERF_POLICY_ON_AC = "powersave";
+			CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+	 		CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 	
 	 		CPU_MIN_PERF_ON_AC = 0;
 	 		CPU_MAX_PERF_ON_AC = 100;
