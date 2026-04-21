c4f14892029d02fbd66c99518640a7a8a3c5748a
felix <ichters.fe@gmail.com>
2026-04-21 11:02:37 +0200

    add parted to system configuration

--- modules/system/filesystems.nix
+++ modules/system/filesystems.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.udisks2.enable = true;
+  
+  environment.systemPackages = with pkgs; [
+    parted
+  ];
 
 #  fileSystems."/mnt/stash" = {
 #    device = "/dev/disk/by-uuid/42d44629-0583-4660-95cf-3a7ceb940954";
